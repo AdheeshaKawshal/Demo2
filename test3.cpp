430815bfdf21a26fef1acb1a57f02b8ee29709bb
AdheeshaKawshal <152969071+AdheeshaKawshal@users.noreply.github.com>
2024-07-14 18:53:20 +0530

    chnaged
    totally changed

--- test3.cpp
+++ test3.cpp
@@ -2,25 +2,14 @@
 #include <vector>
 #include <cmath>
 using namespace std;
-int checker(int num,int step,int p,int val){
-    int p2=p;
-    for (int i=step;i<=10;i++){
-        int v=val+pow(i,2);
-        //cout<<i<<"-";
-        if (v==num){
-             p2=1+p;
-            cout<<p2<<"point";
-           // return step;
-            checker(100,i+1,p2,v);}
-        else if(v<num){
-            checker(100,i+1,p2,v);
-        }
-
+void checker(){
+    for(int i=0;i<5;i++){
+        cout<<i;
     }
 
 }
 int main(){
 
-checker(100,1,0,0);
+checker();
     return 0;
 }
